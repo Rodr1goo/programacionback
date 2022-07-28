ccbcf1f34d13b5eabd55c5147a2ee8af19fa8994
Rodrigo Flores <rodri_flores_1998@hotmail.com>
2022-07-27 22:40:17 -0300

    segundo commit, ya utilizando nodemon

--- clase-6/main.js
+++ clase-6/main.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-    res.end('Hola Mundo')
+    const Hora = new Date().getHours()
+    if(6<=Hora && Hora<=12) {
+        res.end('<h1>Buen día</h1>')
+    } else if(13<=Hora && Hora<=19){
+        res.end('<h1>Buenas tardes</h1>')
+    } else if(20<=Hora && Hora<=5){
+        res.end('<h1>Buenas noches</h1>')
+    }
 })
 
 const createServer = server.listen(8080, ()=>{
